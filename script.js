29f136fc663c1306fa5a3e9a967e4836e1ad4197
Jim058 <78271810+Jim058@users.noreply.github.com>
2024-03-06 21:47:45 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,19 +8,29 @@ let result = document.querySelector('.result')
 
 check.addEventListener('click', function() {
   for (let i = 0; i < inputs.length - 1; i++) {
-    //如果第二沒打勾
-    if (inputs[1].checked == false) {
-      //但是第三個打勾，那就會滅世界
-      if (inputs[2].checked == true) {
-        result.innerHTML = labels[0].textContent
-        return 0
-      } //如果第一個打勾
-    } else if (inputs[0].checked == true) {
+    //1為一組，2、3為一組
+    if (inputs[0].checked) {
+      //是否還有勾選2或3
       if (inputs[1].checked || inputs[2].checked) {
         result.innerHTML = '有什麼障礙?'
+        return 0
+      } else {
+        //只有勾選1的話
+        result.innerHTML = labels[0].textContent
       }
-    } else {
+      //如果3打勾
+    } else if (inputs[2].checked) {
+      //判斷2有沒有打勾
+      if (inputs[1].checked) {
+        result.innerHTML = '幸福美滿的世界'
+      } else {
+        result.innerHTML = labels[0].textContent
+      }
+      //判斷2有沒有打勾
+    } else if (inputs[1].checked){
       result.innerHTML = '幸福美滿的世界'
+    } else {
+      result.innerHTML = '有什麼障礙?'
     }
 
   }
